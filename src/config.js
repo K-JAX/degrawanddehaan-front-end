f006ad533188c7403757eff9cef62fe1004e44d2
Benco_Dental <scroogemcduck@mail.com>
2019-07-24 17:36:25 -0400

    Testing the git deployment again.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // graphql api url
-let url = 'https://degrawanddehaan.wordtestdomain.com/graphql'; 
+let url = 'https://degrawanddehaan.wordtestdomain.com/graphql';  
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
