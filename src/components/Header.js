fcb7ee9b46464b0c97321027083b308363817106
Benco_Dental <scroogemcduck@mail.com>
2019-07-25 19:29:43 -0400

    Fixing submodule structure.

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,6 @@ class Header extends Component {
     return (
       <div className="flex pa1 justify-between nowrap padding bottomborder">
         <div className="flex flex-fixed black">
-          
           <Link to="/" className="ml1 no-underline black">
             Home
           </Link>
